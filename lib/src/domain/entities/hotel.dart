9e0793fa202c561b4898d7f67bfb77d6251a8281
Daniil <danyildyb@gmail.com>
2023-09-18 14:49:38 +0300

    fix: add changes in hotel entities

--- lib/src/domain/entities/hotel.dart
+++ lib/src/domain/entities/hotel.dart
@@ -1,8 +1,25 @@
-class Honel {
-  int id;
-  String name;
-  String adress;
-  int minimalPrice;
-  String priceForIt;
-  int rating;
+abstract class Honel {
+  final int id;
+  final String name;
+  final String adress;
+  final int minimalPrice;
+  final String priceForIt;
+  final int rating;
+  final String ratingName;
+  final List<String> imageUrls;
+  final String description;
+  final List<String> peculiarities;
+
+  Honel({
+    required this.id,
+    required this.name,
+    required this.adress,
+    required this.minimalPrice,
+    required this.priceForIt,
+    required this.rating,
+    required this.ratingName,
+    required this.imageUrls,
+    required this.description,
+    required this.peculiarities,
+  });
 }
