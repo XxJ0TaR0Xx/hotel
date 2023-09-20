ce3ed6613e721c740c36ed06b31b0eb8a60d51c6
Daniil <danyildyb@gmail.com>
2023-09-21 00:31:36 +0300

    chore: delete trash

--- lib/src/domain/repositories/tourist_repository.dart
+++ lib/src/domain/repositories/tourist_repository.dart
@@ -1,17 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:hotel/src/domain/entities/tourist.dart';
+import 'package:hotel/src/domain/usecases/tourist_usecase/create_tourist_usecase.dart';
+import 'package:hotel/src/domain/usecases/tourist_usecase/update_tourist_usecase.dart';
 
 import '../../../core/failures/failure.dart';
 
 abstract class TouristRepository {
-  Future<Either<Failure, Tourist>> createTourist({
-    required String name,
-    required String surname,
-    required DateTime dateOfBirh,
-    required String citizenship,
-    required int passportNumber,
-    required int passportValidityPeriod,
-  });
+  Future<Either<Failure, Tourist>> createTourist({required CreateTouristUseCaseParams createTouristParams});
 
   Future<Either<Failure, Tourist>> findOneTourist({required int id});
 
@@ -19,13 +14,5 @@ abstract class TouristRepository {
 
   Future<Either<Failure, Unit>> deleteTorist({required int id});
 
-  Future<Either<Failure, Tourist>> updateTourist({
-    required id,
-    name,
-    surname,
-    dateOfBirh,
-    citizenship,
-    passportNumber,
-    passportValidityPeriod,
-  });
+  Future<Either<Failure, Tourist>> updateTourist({required UpdateTouristUseCaseParams updateTouristUseCaseParams});
 }
