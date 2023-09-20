8b92e2a03db5d9d2c6aa135b0b7c8e86ff1b32a1
Daniil <danyildyb@gmail.com>
2023-09-21 00:31:12 +0300

    chore: delete trash

--- lib/core/services/servisec.config.dart
+++ lib/core/services/servisec.config.dart
@@ -10,9 +10,19 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:hotel/src/domain/usecases/get_all_hotel_usecase.dart' as _i3;
-import 'package:hotel/src/domain/usecases/get_all_rooms_usecase.dart' as _i4;
-import 'package:hotel/src/domain/usecases/get_booking_usecase.dart' as _i5;
+import 'package:hotel/src/domain/usecases/get_all_hotel_usecase.dart' as _i7;
+import 'package:hotel/src/domain/usecases/get_all_rooms_usecase.dart' as _i8;
+import 'package:hotel/src/domain/usecases/get_booking_usecase.dart' as _i9;
+import 'package:hotel/src/domain/usecases/tourist_usecase/create_tourist_usecase.dart'
+    as _i3;
+import 'package:hotel/src/domain/usecases/tourist_usecase/delete_tourist_usecase.dart'
+    as _i4;
+import 'package:hotel/src/domain/usecases/tourist_usecase/find_all_tourist_usecase.dart'
+    as _i5;
+import 'package:hotel/src/domain/usecases/tourist_usecase/find_tourist_usecase.dart'
+    as _i6;
+import 'package:hotel/src/domain/usecases/tourist_usecase/update_tourist_usecase.dart'
+    as _i10;
 import 'package:injectable/injectable.dart' as _i2;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -26,9 +36,14 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i3.GetAllHotelUseCase>(() => _i3.GetAllHotelUseCase());
-    gh.factory<_i4.GetAllRoomsUseCase>(() => _i4.GetAllRoomsUseCase());
-    gh.factory<_i5.GetBookingUseCase>(() => _i5.GetBookingUseCase());
+    gh.factory<_i3.CreateTouristUsecase>(() => _i3.CreateTouristUsecase());
+    gh.factory<_i4.DeleteTouristUsecase>(() => _i4.DeleteTouristUsecase());
+    gh.factory<_i5.FindAllTouristUsecase>(() => _i5.FindAllTouristUsecase());
+    gh.factory<_i6.FindTouristUsecase>(() => _i6.FindTouristUsecase());
+    gh.factory<_i7.GetAllHotelUseCase>(() => _i7.GetAllHotelUseCase());
+    gh.factory<_i8.GetAllRoomsUseCase>(() => _i8.GetAllRoomsUseCase());
+    gh.factory<_i9.GetBookingUseCase>(() => _i9.GetBookingUseCase());
+    gh.factory<_i10.UpdateTouristUsecase>(() => _i10.UpdateTouristUsecase());
     return this;
   }
 }
--- test/domain/injactor/servises.dart
+++ test/domain/injactor/servises.dart
@@ -1,37 +1,70 @@
+// ignore_for_file: unused_local_variable
+
 import 'dart:async';
 
 import 'package:get_it/get_it.dart';
 import 'package:hotel/src/domain/repositories/booking_repository.dart';
 import 'package:hotel/src/domain/repositories/hotel_repository.dart';
 import 'package:hotel/src/domain/repositories/room_repository.dart';
+import 'package:hotel/src/domain/repositories/tourist_repository.dart';
 import 'package:hotel/src/domain/usecases/get_all_hotel_usecase.dart';
 import 'package:hotel/src/domain/usecases/get_all_rooms_usecase.dart';
 import 'package:hotel/src/domain/usecases/get_booking_usecase.dart';
+import 'package:hotel/src/domain/usecases/tourist_usecase/create_tourist_usecase.dart';
+import 'package:hotel/src/domain/usecases/tourist_usecase/delete_tourist_usecase.dart';
+import 'package:hotel/src/domain/usecases/tourist_usecase/find_all_tourist_usecase.dart';
+import 'package:hotel/src/domain/usecases/tourist_usecase/find_tourist_usecase.dart';
+import 'package:hotel/src/domain/usecases/tourist_usecase/update_tourist_usecase.dart';
 
 import '../test_repositories.mocks.dart';
 import 'arrange/mock_booking_repository.dart';
 import 'arrange/mock_hotel_repository.dart';
 import 'arrange/mock_room_repository.dart';
+import 'arrange/mock_tourist_repository.dart';
 
 final GetIt testServices = GetIt.I;
 
 FutureOr<void> initMockTestServices() {
-  final RoomRepository roomRepository = MockRoomRepository();
-  final BookingRepository bookingRepository = MockBookingRepository();
-  final HotelRepository hotelRepository = MockHotelRepository();
-
   testServices.registerLazySingleton<RoomRepository>(() => arrangeMockRoomRepository());
+  testServices.registerLazySingleton<TouristRepository>(() => arrangeMockTouristRepository());
   testServices.registerLazySingleton<BookingRepository>(() => arrangeMockBookingRepository());
   testServices.registerLazySingleton<HotelRepository>(() => arrangeMockHotelRepository());
 }
 
 FutureOr<void> initUsecaseTestServises() {
+  //! GROUP TOURIST
+  //! for CreateTouristUsecase
+  testServices.registerLazySingleton<CreateTouristUsecase>(() => CreateTouristUsecase());
+
+  //! for UpdateTouristUsecase
+  testServices.registerLazySingleton<UpdateTouristUsecase>(() => UpdateTouristUsecase());
+
+  //! for DeleteTouristUsecase
+  testServices.registerLazySingleton<DeleteTouristUsecase>(() => DeleteTouristUsecase());
+
+  //! for FindOneTouristUsecase
+  testServices.registerLazySingleton<FindTouristUsecase>(() => FindTouristUsecase());
+
+  //! for FindAllTouristUsecase
+  testServices.registerLazySingleton<FindAllTouristUsecase>(() => FindAllTouristUsecase());
+
+  ///////////////////////
+
+  //! GROUP HOTEL
   //! for GetAllHotelUseCase
   testServices.registerLazySingleton<GetAllHotelUseCase>(() => GetAllHotelUseCase());
 
+  ///////////////////////
+
+  //! GROUP ROOM
   //! for GetAllRoomsUseCase
   testServices.registerLazySingleton<GetAllRoomsUseCase>(() => GetAllRoomsUseCase());
 
+  ///////////////////////
+
+  //! GROUP BOOKIN
   //! for GetBookingUseCase
   testServices.registerLazySingleton<GetBookingUseCase>(() => GetBookingUseCase());
+
+  ///////////////////////
 }
