25e345dc639acd4421f1f5043c0b9ddf03d84e21
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-09-27 17:55:18 +0300

    fix: container AboutHotel

--- lib/src/presentation/src/widget/hotel_page_widget/about_hote.dart
+++ lib/src/presentation/src/widget/hotel_page_widget/about_hote.dart
@@ -13,15 +13,20 @@ class AboutHotel extends StatelessWidget {
       children: peculiarities.map((peculiarity) {
         return Padding(
           padding: const EdgeInsets.only(
-            right: 8.0,
+            left: 16.0,
             top: 8.0,
           ),
           child: Container(
             padding: const EdgeInsets.symmetric(
               vertical: 5.0,
-              horizontal: 16.0,
+              horizontal: 10.0,
             ),
-            decoration: const BoxDecoration(color: Color(0xFFFBFBFC)),
+            decoration: const BoxDecoration(
+              color: Color(0xFFFBFBFC),
+              borderRadius: BorderRadius.all(
+                Radius.circular(5.0),
+              ),
+            ),
             child: Text(
               peculiarity,
               style: const TextStyle(color: Color(0xFF828796)),
