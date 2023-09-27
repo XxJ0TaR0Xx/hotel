b74226b5bee7bb4820b5b0da1c01a8e8f0bfe972
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-09-27 17:50:57 +0300

    feat: add check platform

--- lib/src/data/datasource/booking_datasource.dart
+++ lib/src/data/datasource/booking_datasource.dart
@@ -1,15 +1,25 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:hotel/core/services/servisec.dart';
 import 'package:hotel/src/data/models/bookind_model.dart';
 import 'package:hotel/src/data/utils/status_code_hendler.dart';
 import 'package:injectable/injectable.dart';
 
+String isAndroid({required String path}) {
+  if (Platform.isAndroid) {
+    return 'http://192.168.1.71:1337/api/$path/1';
+  } else {
+    return 'http://127.0.0.1:1337/api/$path/1';
+  }
+}
+
 @Injectable()
 class BookingDatasource {
   final DioModule dio;
   BookingDatasource({required this.dio});
 
-  static const String httpBookings = 'http://127.0.0.1:1337/api/bookings/1';
+  String httpBookings = isAndroid(path: 'bookings');
 
   Future<BookingModel> getBookingModel() async {
     try {
