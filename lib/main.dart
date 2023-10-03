6dfe3cf3d9338a08368ac02a00cdda9b9e8eb618
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-10-03 18:35:36 +0300

    feat: add servces in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hotel/core/services/servisec.dart';
 import 'package:hotel/src/presentation/src/app.dart';
 
 Future<void> main() async {
-  runApp(const App());
+  WidgetsFlutterBinding.ensureInitialized();
+  await initServices();
+
+  runApp(App());
 }
