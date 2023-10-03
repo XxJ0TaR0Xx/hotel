f9afdf233c8d6a4759855f5fb9635eb5282e8d3b
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-10-03 18:36:09 +0300

    feat: add cusotmer datasource

---
+++ lib/src/data/datasource/customer_datasource.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:hotel/core/services/servisec.dart';
+import 'package:hotel/src/data/datasource/booking_datasource.dart';
+import 'package:hotel/src/data/models/customer_model.dart';
+import 'package:hotel/src/data/utils/status_code_hendler.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable()
+class CustomerDatasource {
+  final DioModule dio;
+  CustomerDatasource({required this.dio});
+
+  String httpCustomers = isAndroid(path: 'customers');
+
+  Future<CustomerModel> postCustomer({required CustomerModel customerModel}) async {
+    try {
+      final Response response = await dio.clietn.request(
+        httpCustomers,
+        options: Options(
+          method: 'POST',
+        ),
+        data: customerModel.toJson(),
+      );
+
+      StatusCodeHandler.check(response.statusCode.toString());
+
+      return CustomerModel.fromMap(response.data['data']);
+    } catch (_) {
+      rethrow;
+    }
+  }
+}
