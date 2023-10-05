926f89534c08f8bdacedfa64b9858f256c7d3ed4
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-10-05 15:53:16 +0300

    feat: add controllers in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:hotel/core/services/servisec.dart';
 import 'package:hotel/src/presentation/src/app.dart';
+import 'package:hotel/src/presentation/src/controller/hotel_page_controller/hotel_controller.dart';
+import 'package:hotel/src/presentation/src/controller/number_page_controller/number_controller.dart';
+import 'package:hotel/src/presentation/src/controller/reservation_page_controlle/reservation_page_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initServices();
 
-  runApp(App());
+  HotelController hotelController = HotelController();
+  NumberController numberController = NumberController();
+  ReservationController reservationController = ReservationController();
+
+  runApp(App(
+    hotelController: hotelController,
+    numberController: numberController,
+    reservationController: reservationController,
+  ));
 }
